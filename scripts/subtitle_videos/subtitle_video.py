c86755965fd5d3134490a7dd3fcf5faa3ce329b1
aeronesto <ejimenez.mit@gmail.com>
2024-10-05 21:18:30 -0600

    adjustments for sharing on youtube

--- scripts/subtitle_videos/subtitle_video.py
+++ scripts/subtitle_videos/subtitle_video.py
@@ -10,12 +10,8 @@ import os
 from datetime import datetime
 current_time = datetime.now().strftime("%y-%m-%d-%H-%M-%S")
 
-"""
-This script allows you to create captioned videos, replacing the original video with an image.
-"""
-
-def display_lyrics(ig_width, txt):
-
+def display_subtitles(ig_width,txt):
+    
     text_clip = TextClip(
         txt=txt,
         font='P052-Bold', # install other fonts with ImageMagick
@@ -26,80 +22,66 @@ def display_lyrics(ig_width, txt):
         size=(int(ig_width * 0.8), None),  # Fix width and auto-adjust height
         method='caption'  # method to create multiline text
     ).set_position('center')
-
-    txt_width, txt_height = text_clip.size
     
     text_background_clip = ColorClip(
-        size=(int(txt_width*1.1), int(txt_height*1.1)),
-        color=(135, 206, 235)
-    ).set_position('center').set_opacity(0.8)
+        size=text_clip.size,  # set to text box dimensions
+        color=(29, 79, 129) # background color
+    ).set_position('center').set_opacity(0.7)
     
     return CompositeVideoClip([text_background_clip, text_clip])
 
 def subtitle_video(input_video_path, input_image_path):
+    
     tmp_path = "./scripts/subtitle_videos/tmp"
 
-    # Use local clip
     video_basename = os.path.basename(input_video_path)
     video_filename = os.path.splitext(video_basename)[0]
     audio_path = f'{tmp_path}/{current_time}-{video_filename}.mp3'
 
     # load video
-    my_clip = mp.VideoFileClip(input_video_path)
+    video_clip = mp.VideoFileClip(input_video_path)
     
-    # save audio from video
-    my_clip.audio.write_audiofile(audio_path)
+    # extract audio
+    video_clip.audio.write_audiofile(audio_path)
+    
+    # load transcriber model
+    model = whisper.load_model('medium')
 
-    # Instantiate whisper model using model_type variable
-    model_type = 'medium'
-    model = whisper.load_model(model_type)
-    
-    # Get text from speech for subtitles from audio file
+    # transcribe the audio
     result = model.transcribe(
         audio_path,
         task = 'translate',
         word_timestamps=True
         )
-    
-    # create Subtitle dataframe, and save it
+
+    # create segments for phrases
     dict1 = {'start':[], 'end':[], 'text':[]}
-    # Q: what is the format of result['segments']? result['segments']['words'] contains a list of words just like segments contains segments with start, end, text
-    for i in result['segments']:
-        dict1['start'].append(int(i['start']))
-        dict1['end'].append(int(i['end']))
-        dict1['text'].append(i['text'])
+    for segment in result['segments']:
+        dict1['start'].append(segment['start'])
+        dict1['end'].append(segment['end'])
+        dict1['text'].append(segment['text'])
     df = pd.DataFrame.from_dict(dict1)
     df.to_csv(f'{tmp_path}/subs.csv')
-
+    
     # load previously saved subs file
-    # df = pd.read_csv(f'{tmp_path}/subs.csv', header=0, names=['start', 'end', 'text'], dtype={'start': int, 'end': int})
-
-    # load input video
-    video_clip = VideoFileClip(input_video_path)
-    duration = video_clip.duration
+    # df = pd.read_csv(f'{tmp_path}/subs.csv', header=0, names=['start', 'end', 'text'], dtype={'start': float, 'end': float})
 
     # create video with input image
-    image_clip = ImageClip(input_image_path, duration=duration).set_fps(video_clip.fps)
+    image_clip = ImageClip(input_image_path, duration=video_clip.duration).set_fps(video_clip.fps)
+    image_clip_width, image_clip_height = image_clip.size
     
-    ig_width, ig_height = image_clip.size      
-
-    # create and format subtitles with SubtitlesClip
-    generator = partial(display_lyrics, ig_width)
-    # Q: are these cascaded tuple, zip necessary?
-    subs = tuple(zip(tuple(zip(df['start'].values, df['end'].values)), df['text'].values))
+    # create subtitles with segments
+    subs = tuple(zip(zip(df['start'].values, df['end'].values), df['text'].values))
+    generator = partial(display_subtitles, image_clip_width)
     subtitles = SubtitlesClip(subs, generator)
-
-    # create final video with input image and subtitles centered
-    final = CompositeVideoClip([image_clip, subtitles.set_pos(('center'))])
-
-    # set audio from original video
-    final = final.set_audio(video_clip.audio)
     
-    # save final video
-    final.write_videofile(f'{tmp_path}/{current_time}-{video_basename}', fps=image_clip.fps, remove_temp=True, codec="libx264", audio_codec="aac")
+    # create final video of image with subtitles and original audio
+    final_clip = CompositeVideoClip([image_clip, subtitles.set_pos(('center'))])
 
-if __name__ == "__main__":
-    input_image_path = './scripts/subtitle_videos/tmp/lion.png'
-    input_video_path = './scripts/subtitle_videos/tmp/massive_lion.mp4'
-    # dir_name = os.path.splitext(os.path.basename(input_video_path))[0]
-    subtitle_video(input_video_path, input_image_path)
+    final_clip = final_clip.set_audio(video_clip.audio)
+    
+    final_clip.write_videofile(f'{tmp_path}/{current_time}-{video_basename}', fps=image_clip.fps, remove_temp=True, codec="libx264", audio_codec="aac")
+
+input_video_path = './scripts/subtitle_videos/tmp/massive_lion.mp4'
+input_image_path = './scripts/subtitle_videos/tmp/lion.png'
+subtitle_video(input_video_path, input_image_path)
